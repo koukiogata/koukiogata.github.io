de423e39423e9f3025984458f972d964412005e5
koukiogata <110591901+koukiogata@users.noreply.github.com>
2024-09-17 03:18:41 +0900

    portfolio2024/09/017
    portfolio ogatakouki

--- script.js
+++ script.js
@@ -6,77 +6,82 @@ document.addEventListener('DOMContentLoaded', function () {
     const svg = document.querySelector('svg');
     const background = document.getElementById('background');
     const outerContainer = document.getElementById('outer-container')
-
+    
+    // outerContainer.style.display = 'none',
     document.body.style.overflow = 'hidden';
     svg.style.display = 'none';
-
-    // Kのアニメーション定義と実行
-    loading.animate([
-        { transform: 'translateY(100%)' }, // 下から出現
-        { transform: 'translateY(-10%)' }, // 小さく跳ねる
-        { transform: 'translateY(0)' }     // 元の位置に戻る
-    ], {
-        duration: 500, // アニメーションの持続時間
-        fill: 'forwards' // アニメーション終了後のスタイルを維持
-    }).onfinish = function () {
-        // Kのアニメーション完了後にVisualの表示とアニメーションを開始
-        setTimeout(() => {
-            visual.style.display = 'block'; // Visualを表示
-            visual.animate([
-                { transform: 'translateX(-100%)', opacity: 0 }, // スライドイン開始位置（左端から）
-                { transform: 'translateX(0%)', opacity: 1 }     // 終了位置（Kの右隣）
-            ], {
-                duration: 600, // アニメーションの時間
-                fill: 'forwards' // アニメーション終了後のスタイルを維持
-            });
-
-            // KとVisualを一緒に左に30pxスライド
-            textContainer.animate([
-                { transform: 'translateX(0px)' },
-                { transform: 'translateX(-25vw)' }
-            ], {
-                duration: 600, // アニメーションの時間
-                fill: 'forwards' // アニメーション終了後のスタイルを維持
-            }).finished.then(() => {
-                svg.style.display = 'block';
-                // textContainerアニメーション完了後にframeのアニメーションを開始
-                frame.animate([
-                    { strokeDashoffset: 1200 },
-                    { strokeDashoffset: 0 }
+    
+    loading.style.display = 'none' ,
+    setTimeout(() => {
+        // Kのアニメーション定義と実行
+        loading.style.display = 'block' ,
+        loading.animate([
+            { transform: 'translateY(30%)' }, // 下から出現
+            { transform: 'translateY(-50%)' }, // 小さく跳ねる
+            { transform: 'translateY(0)' },    // 元の位置に戻る
+        ], {
+            duration: 500, // アニメーションの持続時間
+            fill: 'forwards' // アニメーション終了後のスタイルを維持
+        }).onfinish = function () {
+            setTimeout(() => {
+                // Kのアニメーション完了後にVisualの表示とアニメーションを開始
+                visual.style.display = 'block'; // Visualを表示
+                visual.animate([
+                    { transform: 'translateX(-100%)', opacity: 0 }, // スライドイン開始位置（左端から）
+                    { transform: 'translateX(0%)', opacity: 1 }     // 終了位置（Kの右隣）
                 ], {
-                    duration: 2000,
-                    fill: 'forwards'
-                }).finished.then(() => {
-                    frame.setAttribute('fill', 'black');
-                    frame.setAttribute('stroke', 'white');
-                    loading.style.color = 'white';
-                    visual.style.color = 'white';
-                    // SVGアニメーション完了後に背景のオパシティを1に変更
-                    background.style.opacity = 1;
-                    background.animate([
-                        { transform: 'scaleY(0)' }, // 中央から始まる拡大
-                        { transform: 'scaleY(1)' } // 画面全体をカバー
-                    ], {
-                        duration: 500,
-                        fill: 'forwards'
-                    });
-                    svg.addEventListener('click', function() {
-                        // outer-containerのフェードアウトアニメーションを設定（中央から透明になっていく）
-                        outerContainer.animate([
-                            { transform: 'scaleY(1)', opacity: 1 }, // 元の大きさから開始
-                            { transform: 'scaleY(1)', opacity: 0 }  // 垂直方向に縮小しながら透明になる
-                        ], {
-                            duration: 500,
-                            easing: 'ease-in-out',
-                            fill: 'forwards'
-                        }).finished.then(() => {
-                            outerContainer.style.display = 'none';
-                            document.body.style.overflow = 'auto'; // アニメーションが完了したら表示を非表示にする
-                        });
-                    });
+                    duration: 600, // アニメーションの時間
+                    fill: 'forwards' // アニメーション終了後のスタイルを維持
                 });
 
-            });
-        }, 1000); // 0.5秒の遅延
-    };
+                // KとVisualを一緒に左に30pxスライド
+                textContainer.animate([
+                    { transform: 'translateX(0px)' },
+                    { transform: 'translateX(-25vw)' }
+                ], {
+                    duration: 600, // アニメーションの時間
+                    fill: 'forwards' // アニメーション終了後のスタイルを維持
+                }).finished.then(() => {
+                    svg.style.display = 'block';
+                    // textContainerアニメーション完了後にframeのアニメーションを開始
+                    frame.animate([
+                        { strokeDashoffset: 1200 },
+                        { strokeDashoffset: 0 }
+                    ], {
+                        duration: 2000,
+                        fill: 'forwards'
+                    }).finished.then(() => {
+                        frame.setAttribute('fill', 'black');
+                        frame.setAttribute('stroke', 'white');
+                        loading.style.color = 'white';
+                        visual.style.color = 'white';
+                        // SVGアニメーション完了後に背景のオパシティを1に変更
+                        background.style.opacity = 1;
+                        background.animate([
+                            { transform: 'scaleY(0)' }, // 中央から始まる拡大
+                            { transform: 'scaleY(1)' } // 画面全体をカバー
+                        ], {
+                            duration: 500,
+                            fill: 'forwards'
+                        });
+                        svg.addEventListener('click', function () {
+                            // outer-containerのフェードアウトアニメーションを設定（中央から透明になっていく）
+                            outerContainer.animate([
+                                { transform: 'scaleY(1)', opacity: 1 }, // 元の大きさから開始
+                                { transform: 'scaleY(1)', opacity: 0 }  // 垂直方向に縮小しながら透明になる
+                            ], {
+                                duration: 500,
+                                easing: 'ease-in-out',
+                                fill: 'forwards'
+                            }).finished.then(() => {
+                                outerContainer.style.display = 'none';
+                            });
+                        });
+                    });
+                    
+                });
+            }, 300); // 0.5秒の遅延
+        };
+    }, 1000);
+    document.body.style.overflow = 'auto'; // アニメーションが完了したら表示を非表示にする
 });
